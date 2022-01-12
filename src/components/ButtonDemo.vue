8fe4d57fc5e98db4b56f831269500112301f8cbb
liql <liqianlong219@gmail.com>
2022-01-12 16:00:11 +0800

    CSS最小影响原则

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,10 +2,10 @@
   <div>示例</div>
   <h1>示例1</h1>
   <div>
-    <button>你好</button>
-    <Button theme = 'button'>你好么，老哥</Button>
-    <Button theme = 'link'>你好么，老哥</Button>
-    <Button theme = 'text'>你好么，老哥</Button>
+    <Button>你好</Button>
+    <Button theme = 'button'>你好</Button>
+    <Button theme = 'link'>你好</Button>
+    <Button theme = 'text'>你好</Button>
   </div>
 </template>
 
--- src/libs/Switch.vue
+++ src/libs/Switch.vue
@@ -1,60 +1,58 @@
 <template>
-  <button  @click="toggle" :class="{checked:value}">
+  <button class="tiger-switch" @click="toggle" :class="{'tiger-checked':value}">
     <span></span>
   </button>
 </template>
 
-<script  lang='ts'>
+<script lang='ts'>
 
 export default {
-  props:{
-    value:Boolean
+  props: {
+    value: Boolean
   },
-  setup(props,context){
-   const  toggle =()=>{
-      context.emit('update:value',!props.value)
-    }
-    return {toggle}
+  setup(props, context) {
+    const toggle = () => {
+      context.emit('update:value', !props.value);
+    };
+    return {toggle};
   }
-}
-
-
-
+};
 </script>
 
-
-<style lang='scss' scoped>
+<style lang='scss'>
 $h: 22px;
 $h2: $h - 4px;
-button{
+.tiger-switch {
   height: $h;
   width: $h*2;
   border: none;
   background: gray;
   border-radius: $h/2;
   position: relative;
-  &:focus{
+
+  &:focus {
     outline: none;
   }
-  > span{
+
+  > span {
     position: absolute;
     top: 2px;
     left: 2px;
     height: $h2;
     width: $h2;
-    background:white;
+    background: white;
     border-radius: 50%;
-    transition:left 0.25s
+    transition: left 0.25s
   }
-  &.checked{
+
+  &.tiger-checked {
     background: blue;
-    > span{
-      left :calc(100% - #{$h2} - 2px)
+
+    > span {
+      left: calc(100% - #{$h2} - 2px)
     }
   }
 }
-
-
 
 
 </style>
