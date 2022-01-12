21685c1bec2b726598ed02585e21855e9fb100a4
liql <liqianlong219@gmail.com>
2022-01-12 15:21:22 +0800

    添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,13 +1,19 @@
 <template>
+  <div>示例</div>
+  <h1>示例1</h1>
   <div>
-    <Button />
+    <button>你好</button>
+    <Button theme = 'button'>你好么，老哥</Button>
+    <Button theme = 'link'>你好么，老哥</Button>
+    <Button theme = 'text'>你好么，老哥</Button>
   </div>
 </template>
 
 <script setup lang='ts'>
-
-
 import Button from '../libs/Button.vue';
+const onClick =()=>{
+  console.log('fuck');
+}
 </script>
 
 <style lang='scss' scoped>
--- src/libs/Button.vue
+++ src/libs/Button.vue
@@ -1,14 +1,28 @@
 <template>
-  <button>
+  <button class="tiger-button" :class=" `theme-${theme}` ">
     <slot />
   </button>
+
 </template>
 
 <script setup lang='ts'>
 
+import {defineProps} from 'vue';
 
+const props = defineProps({
+  theme:{
+    type: String,
+    default:'button'
+  }
+})
 </script>
 
 <style lang='scss' scoped>
 
+.box{
+  width: 300px;
+  height: 300px;
+  background: #bfc;
+  padding: 20px;
+}
 </style>
