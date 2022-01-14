c3a4625031385b4444411dcb0b48868adaad0201
liql <liqianlong219@gmail.com>
2022-01-14 09:36:02 +0800

    创建dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
+    <Dialog>
   </div>
 </template>
 
 <script setup lang='ts'>
 
 
+import Dialog from '../libs/Dialog.vue';
 </script>
 
 <style lang='scss' scoped>
--- src/libs/Dialog.vue
+++ src/libs/Dialog.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <div class="dialog-voerlay"></div>
+    <div class="dialog-warpper"></div>
+    <header>标题</header>
+    <main>
+      <p>第一行</p>
+      <p>第二行</p>
+    </main>
+    <footer>
+      <Button>Ok</Button>
+      <Button>Cancel</Button>
+    </footer>
   </div>
 </template>
 
