28ca3692f512e3eb5ec9c532eaf42cf8e2c31cb4
liql <liqianlong219@gmail.com>
2022-01-15 15:38:12 +0800

    渲染tabs内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>示例 </div>
+  <div>tabs 示例 </div>
   <h1>示例1</h1>
   <Tabs>
-    <Tab title="导航1">内容1</Tab>>
-    <Tab title="导航2">内容2</Tab>>
-  </Tabs>>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
 
 <script setup lang='ts'>
 
 
-import Tabs from '../libs/tabs.vue';
+import Tabs from '../libs/Tabs.vue';
 import Tab from '../libs/Tab.vue';
 </script>
 
--- src/libs/Tabs.vue
+++ src/libs/Tabs.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
+    <div v-for="(t,index) in title" :key="index">{{t}}</div>
+    <component v-for='(c,index) in defaults ' :is="c" :key="index"/>
   </div>
 </template>
 
 <script setup lang='ts'>
+import Tab from './Tab.vue';
+import {useSlots} from 'vue';
 
-
+const defaults = useSlots().default();
+defaults.forEach((tag) => {
+  if (tag.type !== Tab) {
+    throw new Error('Tabs 子标签必须是Tab');
+  }
+});
+let title = defaults.map((tag) => {
+  return tag.props.title;
+});
 </script>
 
 <style lang='scss' scoped>
